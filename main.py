53ba8f02720f4586cc2eaf0ecf8d06e05dca6c31
DnA2702 <16520024@std.stei.itb.ac.id>
2023-04-13 20:26:44 +0700

    GUI

--- main.py
+++ main.py
@@ -1,45 +1,149 @@
+import sys
+from tkinter import Widget
+from PyQt5.QtWidgets import QFileDialog, QApplication, QWidget, QMainWindow, QMessageBox
+from PyQt5.uic import loadUi
+from PyQt5 import QtWidgets, QtGui, QtCore
+from PyQt5.QtGui import QPixmap
+
 from RSA import *
 
-p = generatePrima()
-q = generatePrima()
-n = p * q
-totient = (p - 1) * (q - 1)
-while True:
-    e = generatePrima()
-    if (totient % e != 0):
-        break
-d = cariD(totient, e)
-m = "92dad9443e4dd6d70a7f11872101ebff87e21798e4fbb26fa4bf590eb440e71b"
+import sqlite3
+import os
+import csv
+import datetime
+import random
 
-print('')
-print("p: " + str(p))
-print('')
-print("q: " + str(q))
-print('')
-print("n: " + str(n))
-print('')
-print("totient: " + str(totient))
-print('')
-print("e: " + str(e))
-print('')
-print("d: " + str(d))
+class Menu(QMainWindow):
+    def __init__(self):
+        super(Menu, self).__init__()
+        loadUi("main.ui", self)
+        self.pushButton_5.clicked.connect(self.GenKey)
+        self.pushButton_6.clicked.connect(self.DigitalSign)
+        self.pushButton_7.clicked.connect(self.SignVerif)
 
-print('')
-print("ENKRIPSI")
-print('')
-c = enkripsi(m, e, n)
-print("Hasil enkripsi: ")
-print(c)
+    def DigitalSign(self):
+        digitalSign = DigitalSign()
+        widget.addWidget(digitalSign)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
 
-print('')
-print("DEKRIPSI")
-print('')
-m = dekripsi (c, d, n)
-print("Hasil dekripsi: ")
-print(m)
+    def GenKey(self):
+        genKey = GenKey()
+        widget.addWidget(genKey)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
 
-kunciPublik = [e, n]
-kunciPrivat = [d, n]
+    def SignVerif(self):
+        signVerif = SignVerif()
+        widget.addWidget(signVerif)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
 
-print(kunciPublik)
-print(kunciPrivat)
+class GenKey(QMainWindow):
+    def __init__(self):
+        super(GenKey, self).__init__()
+        loadUi("genkey.ui", self)
+        self.pushButton_10.clicked.connect(self.Menu)
+        self.pushButton_11.clicked.connect(self.PopUpGenerate)
+    
+    def Menu(self):
+        menu = Menu()
+        widget.addWidget(menu)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
+
+    def PopUpGenerate(self):
+        namaFile = self.textEdit.toPlainText()
+        if (namaFile == ""):
+            msg = QMessageBox()
+            msg.setWindowTitle("Notification")
+            msg.setText("Nama File Masih Kosong Say :)")
+            msg.setIcon(QMessageBox.Warning)
+            x = msg.exec_()
+        else:
+            msg = QMessageBox()
+            msg.setWindowTitle("Notification")
+            msg.setText("File Public dan Private Key Berhasil Tersimpan")
+            msg.setIcon(QMessageBox.Information)
+            x = msg.exec_()
+        # Warning
+        # Critical
+        # Information
+        # Question
+
+class DigitalSign(QMainWindow):
+    def __init__(self):
+        super(DigitalSign, self).__init__()
+        loadUi("digitalsign.ui", self)
+        self.pushButton_10.clicked.connect(self.Menu)
+        self.pushButton_11.clicked.connect(self.AddFile)
+        self.pushButton_12.clicked.connect(self.AddPrivKeyFile)
+        self.pushButton_13.clicked.connect(self.PopUpSign)
+    
+    def Menu(self):
+        menu = Menu()
+        widget.addWidget(menu)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
+
+    def AddFile(self):
+        print("Ayo Clubbing")
+
+    def AddPrivKeyFile(self):
+        print("Ayo Clubbing")
+
+    def PopUpSign(self):
+        msg = QMessageBox()
+        msg.setWindowTitle("Notification")
+        msg.setText("Bla bla bla")
+        msg.setIcon(QMessageBox.Critical)
+        # Warning
+        # Critical
+        # Information
+        # Question
+
+        x = msg.exec_()
+
+class SignVerif(QMainWindow):
+    def __init__(self):
+        super(SignVerif, self).__init__()
+        loadUi("signverif.ui", self)
+        self.pushButton_10.clicked.connect(self.Menu)
+        self.pushButton_11.clicked.connect(self.AddFile)
+        self.pushButton_12.clicked.connect(self.AddPubKeyFile)
+        self.pushButton_13.clicked.connect(self.AddSignedFile)
+        self.pushButton_14.clicked.connect(self.PopUpVerif)
+
+    def Menu(self):
+        menu = Menu()
+        widget.addWidget(menu)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
+
+    def AddFile(self):
+        print("Ayo Clubbing")
+
+    def AddPubKeyFile(self):
+        print("Ayo Clubbing")
+
+    def AddSignedFile(self):
+        print("Ayo Clubbing")
+
+    def PopUpVerif(self):
+        msg = QMessageBox()
+        msg.setWindowTitle("Notification")
+        msg.setText("Bla bla bla")
+        msg.setIcon(QMessageBox.Critical)
+        # Warning
+        # Critical
+        # Information
+        # Question
+
+        x = msg.exec_()
+
+# main
+app = QApplication(sys.argv)
+welcome = Menu()
+widget = QtWidgets.QStackedWidget()
+widget.addWidget(welcome)
+widget.setFixedHeight(600)
+widget.setFixedWidth(800)
+widget.show()
+try:
+    sys.exit(app.exec_())
+except:
+    print("Exit Program")
---
+++ test.py
@@ -0,0 +1,14 @@
+from RSA import *
+
+print(pow(72101, 79, 3337))
+print(pow(3072, 1019, 3337))
+
+print(pow(704, 79, 3337))
+print(pow)
+
+print(967 * 227)
+print((967 - 1) * (227 - 1))
+print(cariD(218316, 79))
+
+print(pow(72101, 79, 219509))
+print(pow(155586, 5527, 219509))
