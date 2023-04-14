82f6aced452618d3790cad950c8adb53e4e2d7e8
brandon jonathan <brandonjonathan7320@gmail.com>
2023-04-14 14:37:00 +0700

    xxx

--- test.py
+++ test.py
@@ -1,14 +1,8 @@
-from RSA import *
+from main import *
 
-print(pow(72101, 79, 3337))
-print(pow(3072, 1019, 3337))
+# misalkan x = pesan yang udah di sign 
+# x dipetakan untuk mengambil message, tanpa digital signaturenya
+# message di-hash
+# digital signature di-dekripsi
+# komparasi hasil hash message dan hasil dekripsi digital signature
 
-print(pow(704, 79, 3337))
-print(pow)
-
-print(967 * 227)
-print((967 - 1) * (227 - 1))
-print(cariD(218316, 79))
-
-print(pow(72101, 79, 219509))
-print(pow(155586, 5527, 219509))
